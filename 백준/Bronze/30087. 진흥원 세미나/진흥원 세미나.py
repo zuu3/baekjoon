ada554c7cc379e1ec36651f8a2b4650ca2e1647c
주현 <ojh27467242@gmail.com>
2024-10-07 08:00:09 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+
+for i in range(N) : 
+    name = input()
+    
+    if name == 'Algorithm' : 
+        print('204')
+    elif name == 'DataAnalysis' :
+        print('207')
+    elif name == 'ArtificialIntelligence' :
+        print('302')
+    elif name == 'CyberSecurity' :
+        print('B101')
+    elif name == 'Network' :
+        print('303')
+    elif name == 'Startup' :
+        print('501')
+    elif name == 'TestStrategy' :
+        print('105')
